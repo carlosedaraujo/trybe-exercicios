4948b19bbbe607b63e8f2b800e23748f4e90126a
carlosedaraujo <carlosed.araujo@gmail.com>
2022-09-26 13:29:42 -0300

    exercicios4.3

---
+++ fundamentos/exercios4-3.js
@@ -0,0 +1,51 @@
+let fat = 1;
+
+for(index = 10; index > 0; index = index -1) {
+
+    fat = fat * index;
+    console.log(fat);
+    
+}
+
+/*let word = 'escada';
+let contrario = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  contrario += word[word.length - 1 - index];
+}
+console.log(word.length);
+console.log(contrario);
+
+
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);
+
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};*/
